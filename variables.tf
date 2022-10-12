eafbb7591ab842bcb52f52afbee7b8b35f16bd30
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-12 13:15:10 +0530

    updated variables info

--- variables.tf
+++ variables.tf
@@ -1,1 +1,56 @@
 
+variable "resource_group_name" {
+   description = "Required name of the resource group"
+   type        = string 
+}
+variable "name" {
+   description = "Name of the security group"
+   default     = "cloud-security-group"
+}
+
+variable "security_rule_name" {
+   description = "Name of the virtual network"
+   default     = "cloud-vnet"
+}
+variable "security_priority" {
+   description = "Priority rule, values are between 100 and 4096"
+   default     = "100"
+}
+variable "security_direction" {
+   description = "Traffic direction incoming or outgoing"
+   default     = "inbound"
+}
+variable "security_access" {
+   description = "Access is allowed or deny"
+   default     = "allow"
+}
+variable "security_protocol" {
+   description = "Name of the protocol"
+   default     = "tcp"
+}
+variable "security_source_port" {
+   description = "Source Ports"
+   default     = "*"
+  type         = string
+}
+variable "secutiry_destination_port" {
+   description = "Destination ports"
+   default     = ["80, "443", "22"]
+}
+
+variable "security_source_address_prefix" {
+   description = "IP address of source"
+   default     = "*"
+ 
+}
+variable "security_destination_address_prefix" {
+   description = "IP address of destination"
+   default     = "*"
+ 
+}
+
+
+
+
+
+
