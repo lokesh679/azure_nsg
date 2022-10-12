b0a34d90257b46e7a1e73e12c9cfd990452d1478
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-12 13:21:41 +0530

    formatting code

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+data "azurerm_resource_group" "rg"{
+  name = var.resource_group_name
+}
 
+resource "azurerm_network_security_group" "az_security_group"{
+  name                = var.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  
+  security_rule {
+    name                       = var.security_rule_name
+    direction                  = var.security_direction
+    access                     = var.security_access
+    protocol                   = var.security_protocol
+    source_port_range          = var.security_source_port
+    destination_port_ranges    = var.security_destination_port
+    source_address_prefix      = var.security_source_address_prefix
+    destination_address_prefix = var.security_destination_address_prefix
+  }
+}
